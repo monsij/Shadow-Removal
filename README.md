e62d29ef972b676191e624ee5a4871c126db170c
monsijb <mbiswal@ucsb.edu>
2022-02-25 14:15:00 -0800

    Add baseline implementation and dataset

---
+++ README.md
@@ -0,0 +1,2 @@
+> Filesystem to be revised with proper names
+> Setup environment such that notebooks can be executed in cloud
---
+++ document_shadow_removal/helper_functions.py
@@ -0,0 +1,33 @@
+"""Helper functions for the document shadow.
+
+Defines helper functions that are used in the document shadow
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def show_img_compar(img_1, img_2, title1, title2):
+    """Show the comparison of two images.
+
+    Input
+    ----------
+    img_1,img_2: numpy array
+    title_1,title_2: str
+
+    Output
+    ----------
+    Two image plots are shown side by side.
+    """
+    f, ax = plt.subplots(1, 2, figsize=(6, 6))
+    ax[0].imshow(img_1)
+    ax[0].set_title(title1)
+    ax[0].axis('off')
+    
+    ax[1].imshow(img_2)
+    ax[1].set_title(title2)
+    ax[1].axis('off')
+
+    f.tight_layout()
+    plt.show()
